ccd15c7b16c8fd0dd9a2a90d01128a30c5ec574b
John Munson <19480078+johncmunson@users.noreply.github.com>
2025-09-25 22:26:56 +0000

    feat: configure remote image patterns using Vercel blob domain

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next"
+import { getEnvVar } from "@/lib/utils"
+
+const vercelBlobDomain = getEnvVar("VERCEL_BLOB_DOMAIN")
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [new URL(`${vercelBlobDomain}/**`)],
+  },
   experimental: {
     serverActions: {
       bodySizeLimit: "10mb",
